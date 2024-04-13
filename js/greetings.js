cf2d2d4950a164d13361e3910a7847ed15e61505
hukudome <koreaitdeveloper@gmail.com>
2024-04-13 15:52:23 -0700

    edit Clock and Greeting

--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,8 @@
 const greeting = document.querySelector('#greeting');
 const loginForm = document.querySelector('#login-form');
+const loginMessage = document.querySelector('#login-message');
 const loginInput = document.querySelector('#login-form input');
+const loginBox = document.querySelector('#loginBox');
 
 const HIDDEN_CLASSNAME = 'hidden';
 const USERNAME_KEY = 'userName';
@@ -8,13 +10,15 @@ const USERNAME_KEY = 'userName';
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
+  loginBox.classList.add(HIDDEN_CLASSNAME);
+  loginMessage.classList.add(HIDDEN_CLASSNAME);
   const userName = loginInput.value;
   localStorage.setItem(USERNAME_KEY, userName);
   paintGreetings(userName);
 }
 
 function paintGreetings(userName) {
-  greeting.innerText = `Hello ${userName}`;
+  greeting.innerText = `Good Afternoon, ${userName}`;
   const loginElementList = document.getElementsByClassName('login-shown');
   Array.from(loginElementList).forEach((element) =>
     element.classList.remove(HIDDEN_CLASSNAME)
@@ -26,6 +30,8 @@ const savedUserName = localStorage.getItem(USERNAME_KEY);
 
 if (!savedUserName) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginBox.classList.remove(HIDDEN_CLASSNAME);
+  loginMessage.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener('submit', onLoginSubmit);
 } else {
   paintGreetings(savedUserName);
